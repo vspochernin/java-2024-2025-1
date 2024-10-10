864fdb0b58185c5de5e35a56c9666429493a2569
Vladislav Pochernin <vspochernin@gmail.com>
2024-10-10 22:44:20 +0300

    lab-6.9: implement key file

---
+++ lab-6.9/src/main/java/ru/vspochernin/KeyReaderUtils.java
@@ -0,0 +1,52 @@
+package ru.vspochernin;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * @author pochernin-vla
+ */
+public class KeyReaderUtils {
+
+    public static String readKeyFromFile(String fileName) {
+        Path keyFile = Paths.get(fileName);
+
+        validateFile(keyFile);
+        String key = readKey(keyFile);
+        validateKey(key, keyFile);
+
+        return key;
+    }
+
+    private static void validateFile(Path keyFile) {
+        if (!Files.exists(keyFile)) {
+            throw new RuntimeException("File not found: " + keyFile.toAbsolutePath());
+        }
+        if (!Files.isRegularFile(keyFile)) {
+            throw new RuntimeException("File is not a regular file: " + keyFile.toAbsolutePath());
+        }
+        if (!Files.isReadable(keyFile)) {
+            throw new RuntimeException("File is not readable: " + keyFile.toAbsolutePath());
+        }
+    }
+
+    private static String readKey(Path keyFile) {
+        String key;
+        try (BufferedReader br = new BufferedReader(new FileReader(keyFile.toFile()))) {
+            key = br.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading key file: " + e.getMessage());
+        }
+        return key;
+    }
+
+    private static void validateKey(String key, Path keyFile) {
+        if (key == null || key.isEmpty()) {
+            throw new RuntimeException("Key file is empty: " + keyFile.toAbsolutePath());
+        }
+    }
+}
--- lab-6.9/src/main/java/ru/vspochernin/Main.java
+++ lab-6.9/src/main/java/ru/vspochernin/Main.java
@@ -4,7 +4,12 @@ package ru.vspochernin;
  * @author pochernin-vla
  */
 public class Main {
+
+    // Key file storing secret key from https://yandex.ru/pogoda/b2b/console/api-page.
+    private static final String KEY_FILE_NAME = "key.txt";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String key = KeyReaderUtils.readKeyFromFile(KEY_FILE_NAME);
+        System.out.println(key);
     }
 }
